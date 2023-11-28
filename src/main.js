45bbeceefc0a136e1075f2778f77995c9fdae722
eldarlrd <eldarlrd@pm.me>
2023-11-28 13:53:01 +0400

    init: First setup

---
+++ src/main.js
@@ -0,0 +1,21 @@
+/**
+ * @license AGPL-3.0-only
+ * JavaScript Data Structures - Manual and Explorable JavaScript Data Structures
+ * Copyright (C) 2023 Eldar Pashazade <eldarlrd@pm.me>
+ *
+ * This file is part of JavaScript Data Structures.
+ *
+ * JavaScript Data Structures is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * JavaScript Data Structures is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with JavaScript Data Structures. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import LinkedList from './data-structures/linked-list/index.js';
