fb1ffc97cd1325599957b3039d9a6a3593088b3b
eldarlrd <eldarlrd@pm.me>
2023-11-28 14:27:27 +0400

    config: rm browser

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 /**
- * @license AGPL-3.0-only
+ * @license GPL-3.0-only
  * JavaScript Data Structures - Manual and Explorable JavaScript Data Structures
  * Copyright (C) 2023 Eldar Pashazade <eldarlrd@pm.me>
  *
  * This file is part of JavaScript Data Structures.
  *
  * JavaScript Data Structures is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3.
  *
  * JavaScript Data Structures is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
+ * You should have received a copy of the GNU General Public License
  * along with JavaScript Data Structures. If not, see <https://www.gnu.org/licenses/>.
  */
 
