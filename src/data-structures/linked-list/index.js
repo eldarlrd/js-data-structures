eb94a127b485588b2ab1186c2c6c4fc0ec3402d7
eldarlrd <eldarlrd@pm.me>
2023-11-30 17:44:00 +0400

    feat: structure

--- src/data-structures/linked-list/index.js
+++ src/data-structures/linked-list/index.js
@@ -4,4 +4,28 @@ export default class LinkedList {
   constructor() {
     this.listHead = null;
   }
+
+  append(value) {}
+
+  prepend(value) {}
+
+  size() {}
+
+  head() {}
+
+  tail() {}
+
+  at(index) {}
+
+  pop() {}
+
+  contains(value) {}
+
+  find(value) {}
+
+  toString() {}
+
+  insertAt(value, index) {}
+
+  removeAt(index) {}
 }
