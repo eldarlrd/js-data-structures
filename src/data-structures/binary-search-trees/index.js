0e579b021a17e7130485dab0e8b200adc1b23c7b
eldarlrd <eldarlrd@pm.me>
2023-12-03 23:19:43 +0400

    feat(bst): sorting & order

--- src/data-structures/binary-search-trees/index.js
+++ src/data-structures/binary-search-trees/index.js
@@ -14,25 +14,6 @@ export default class BinarySearchTrees {
     this.root = this.buildTree(sortedArr);
   }
 
-  #next(nodeChild) {
-    let rootNode = nodeChild;
-    while (rootNode.left) rootNode = rootNode.left;
-    return rootNode;
-  }
-
-  #remove(rootNode) {
-    if (rootNode.left && rootNode.right) {
-      const nextNode = this.#next(rootNode.right);
-      rootNode.value = nextNode.value;
-      rootNode.right = this.delete(nextNode.value, rootNode.right);
-      return rootNode;
-    } else {
-      const swapNode = rootNode.right || rootNode.left;
-      rootNode = null;
-      return swapNode;
-    }
-  }
-
   buildTree(sortedArr) {
     if (sortedArr.length === 0) return null;
 
@@ -62,5 +43,75 @@ export default class BinarySearchTrees {
     if (rootNode.value > value)
       rootNode.left = this.delete(value, rootNode.left);
     else rootNode.right = this.delete(value, rootNode.right);
+  }
+
+  find(value, rootNode = this.root) {
+    if (rootNode === null || rootNode.value === value) return rootNode;
+
+    if (rootNode.value > value) return this.find(value, rootNode.left);
+    else return this.find(value, rootNode.right);
+  }
+
+  levelOrder(callback) {
+    const queue = [this.root];
+    const levelOrderArr = [];
+    while (queue.length > 0) {
+      const rootNode = queue.shift();
+      callback ? callback(rootNode) : levelOrderArr.push(rootNode.value);
+
+      const queueArr = [rootNode?.left, rootNode?.right].filter(value => value);
+
+      queue.push(...queueArr);
+    }
+    if (levelOrderArr.length > 0) return levelOrderArr;
+  }
+
+  inOrder(callback, rootNode = this.root, inOrderArr = []) {
+    if (rootNode === null) return;
+
+    this.inOrder(callback, rootNode.left, inOrderArr);
+    callback ? callback(rootNode) : inOrderArr.push(rootNode.value);
+    this.inOrder(callback, rootNode.right, inOrderArr);
+
+    if (inOrderArr.length > 0) return inOrderArr;
+  }
+
+  preOrder(callback, rootNode = this.root, preOrderArr = []) {
+    if (rootNode === null) return;
+
+    callback ? callback(rootNode) : preOrderArr.push(rootNode.value);
+    this.preOrder(callback, rootNode.left, preOrderArr);
+    this.preOrder(callback, rootNode.right, preOrderArr);
+
+    if (preOrderArr.length > 0) return preOrderArr;
+  }
+
+  postOrder(callback, rootNode = this.root, postOrderArr = []) {
+    if (rootNode === null) return;
+
+    this.postOrder(callback, rootNode.left, postOrderArr);
+    this.postOrder(callback, rootNode.right, postOrderArr);
+    callback ? callback(rootNode) : postOrderArr.push(rootNode.value);
+
+    if (postOrderArr.length > 0) return postOrderArr;
+  }
+
+  #next(nodeChild) {
+    let rootNode = nodeChild;
+    while (rootNode.left) rootNode = rootNode.left;
+    return rootNode;
+  }
+
+  #remove(rootNode) {
+    if (rootNode.left && rootNode.right) {
+      const nextNode = this.#next(rootNode.right);
+      rootNode.value = nextNode.value;
+      rootNode.right = this.delete(nextNode.value, rootNode.right);
+      return rootNode;
+    } else {
+      const swapNode = rootNode.right || rootNode.left;
+      rootNode = null;
+      return swapNode;
+    }
   }
 }
